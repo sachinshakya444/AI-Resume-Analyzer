a025606fa61126d89ad6a781ff4668770ce8bca8
Sachin Shakya <sachinshakya613@gmail.com>
2026-05-28 16:44:36 +0530

    fix cors vercel url

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors({ origin: [
   'http://localhost:5173',
-  'https://ai-resume-analyzer-ivory-seven.vercel.app'
+  'https://ai-resume-analyzer-rxfn.onrender.com'
 ],
 credentials: true
  }))
